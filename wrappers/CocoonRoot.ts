b763626408ec1031c8d17df2c01ad2e0dbf51c78
andreypfau <andreypfau@gmail.com>
2026-04-21 11:15:28 +0900

    Add getData and getCurParams methods to CocoonRoot wrapper

--- wrappers/CocoonRoot.ts
+++ wrappers/CocoonRoot.ts
@@ -455,6 +455,42 @@ export class CocoonRoot implements Contract {
         return res.stack.readNumber() !== 0;
     }
     
+    async getData(provider: ContractProvider) {
+        const res = await provider.get('get_cocoon_data', []);
+        return {
+            version: res.stack.readNumber(),
+            lastProxySeqno: res.stack.readNumber(),
+            paramsVersion: res.stack.readNumber(),
+            uniqueId: res.stack.readNumber(),
+            isTest: res.stack.readNumber(),
+            pricePerToken: res.stack.readNumber(),
+            workerFeePerToken: res.stack.readNumber(),
+            minProxyStake: res.stack.readNumber(),
+            minClientStake: res.stack.readNumber(),
+            ownerAddress: res.stack.readAddress(),
+        };
+    }
+
+    async getCurParams(provider: ContractProvider) {
+        const res = await provider.get('get_cur_params', []);
+        return {
+            paramsVersion: res.stack.readNumber(),
+            uniqueId: res.stack.readNumber(),
+            isTest: res.stack.readNumber(),
+            pricePerToken: res.stack.readBigNumber(),
+            workerFeePerToken: res.stack.readBigNumber(),
+            cachedTokensPriceMultiplier: res.stack.readNumber(),
+            reasoningTokensPriceMultiplier: res.stack.readNumber(),
+            proxyDelayBeforeClose: res.stack.readNumber(),
+            clientDelayBeforeClose: res.stack.readNumber(),
+            minProxyStake: res.stack.readBigNumber(),
+            minClientStake: res.stack.readBigNumber(),
+            proxyScHash: res.stack.readBigNumber(),
+            workerScHash: res.stack.readBigNumber(),
+            clientScHash: res.stack.readBigNumber(),
+        };
+    }
+
     async getAllParams(provider: ContractProvider) {
         const state = await provider.getState();
 
